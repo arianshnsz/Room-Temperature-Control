c2e82b2d0dd660a04788c86456e26daf7c9b1246
arianshnsz <arianshnsz@gmail.com>
2023-10-17 17:09:56 +0330

    temp_control model is there

--- temp_control/models.py
+++ temp_control/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    cooler_status = models.BooleanField(default=False)
+    heater_status = models.BooleanField(default=False)
+    is_automatic = models.BooleanField(default=False)
+
+
+class Weather(models.Model):
+    city_temperature = models.IntegerField()
+    city_weather_condition = models.CharField(max_length=200)
